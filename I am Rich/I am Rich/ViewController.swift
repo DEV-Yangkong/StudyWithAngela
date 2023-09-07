bb6929a4f83bc0cc4246503b40f2a5424a8220b1
yangkong <dev.yangkong@gmail.com>
2023-09-07 16:48:27 +0900

    Create I am Rich

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by yangkong on 2023/09/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
